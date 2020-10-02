6304d437686f2262f2abdbe3b5d57b0a782a073f
Jacob Cowan <mrjcowman@gmail.com>
2020-10-01 17:07:52 -0700

    Build basic string pattern template

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
+// const template = `# ${fullName}
+
+// ${description}
+
+// [Deployed Site: ${deployedURL}](${deployedURL})
+
+// ![Screencap](screencap.png)
+
+// ## Table of Contents
+// 1. [Installation Instructions](#installation-instructions)
+// 2. [Usage](#usage)
+// 3. [License](#license)
+// 4. [Contributing Guidelines](#contributing-guidelines)
+// 5. [Tests](#tests)
+// 6. [Status](#status)
+// 7. [Credits](#credits)
+// 8. [Questions](#questions)
+
+// ## Installation Instructions
+
+// ${installation}
+
+// ## Usage
+
+// \`\`\`
+// ${usage}
+// \`\`\`
+
+// ### License
+
+// ![GitHub](https://img.shields.io/github/license/${user}/${repo}?style=for-the-badge)
+
+// ## Contributing Guidelines
+
+// ${contributing}
+
+// ### Tests
+
+// \`\`\`
+// ${tests}
+// \`\`\`
+
+// ## Status
+
+// ![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/${user}/${repo}?include_prereleases&style=for-the-badge)
+// ![GitHub (Pre-)Release Date](https://img.shields.io/github/release-date-pre/${user}/${repo}?style=for-the-badge)
+// ![GitHub last commit](https://img.shields.io/github/last-commit/${user}/${repo}?style=for-the-badge)
+// ![GitHub commits since latest release (by date including pre-releases)](https://img.shields.io/github/commits-since/${user}/${repo}/latest?include_prereleases&style=for-the-badge)
+
+// ## Credits
+
+// ${credits}
+
+// ## Questions
+
+// ${questions}
+
+// `
+
+// ============================== CODE START ==============================
+
 const fs = require("fs");
 
 // TODO: Implement file reading for faster readme generation
